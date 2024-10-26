8ed308111ae6cc7251cf006728f19f817ef9d84b
splexas <official.fluk3@gmail.com>
2024-10-26 21:59:45 +0300

    feat: header struct refactor: protocol.md

---
+++ vcmp/include/vcmp/protocol.h
@@ -0,0 +1,15 @@
+#ifndef _VCMP_PROTOCOL_H
+#define _VCMP_PROTOCOL_H
+
+#include <cstdint>
+
+typedef struct vcmp_header {
+    uint8_t magic[5];
+    uint8_t version;
+    uint64_t message_uuid[2];
+    uint64_t timestamp;
+    uint8_t sender_key[32];
+    uint32_t payload_size;
+} __attribute__((packed)) vcmp_header_t;
+
+#endif
