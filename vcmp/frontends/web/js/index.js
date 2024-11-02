3f91adb616514883026d9eec426c6384d29f4151
RaphtikAtGHG <raphael.wegmann81@gmail.com>
2024-11-02 21:14:29 +0100

    feat: added message parsing

--- vcmp/frontends/web/js/index.js
+++ vcmp/frontends/web/js/index.js
@@ -4,10 +4,56 @@ const chatLog = document.getElementById("chatLog");
 const messageInput = document.getElementById("messageInput");
 const sendButton = document.getElementById("sendButton");
 const modeToggle = document.getElementById("modeToggle");
+const membersList = document.getElementById("membersList");
+
+function addMember(username) {
+    const existingMember = document.querySelector(`#membersList li[data-username="${username}"]`);
+    
+    if (!existingMember) {
+        const listItem = document.createElement("li");
+        listItem.textContent = username;
+        listItem.dataset.username = username;
+        membersList.appendChild(listItem);
+    }
+}
+
+function removeMember(username) {
+    const existingMember = document.querySelector(`#membersList li[data-username="${username}"]`);
+    
+    if (existingMember) {
+        existingMember.remove();
+    }
+}
+
+function parseData(data) {
+    try {
+        const pdata = JSON.parse(data);  // Parse JSON data
+        console.log("Message from server:", data);
+
+        if (!pdata.action) {
+            console.error("Data is not found in the Message!");
+        }
+        
+        if (pdata.action == "user_join") {
+            addMember(pdata.username);
+        } else if (pdata.action == "user_leave") {
+            removeMember(pdata.username);
+        }
+
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
+    }
+}
 
 
 socket.addEventListener("open", (event) => {
-    console.log("Connected!!");
+    addMember("You");
+    console.log("Websockets opened");
+
+    // example event
+    parseData('{"action": "user_join", "username": "test"}');
+    parseData('{"action": "user_join", "username": "test2"}')
+    //parseData('{"action": "user_leave", "username": "test"}');
 });
 socket.addEventListener("message", (event) => {
     console.log("Message from server ", event.data);
@@ -16,7 +62,8 @@ socket.addEventListener("error", (event) => {
     console.error("WebSocket error observed:", event);
 });
 socket.addEventListener("close", (event) => {
-    console.log("Connection closed:", event);
+    removeMember("You");
+    console.log("Websockets closed");
 });
 
 sendButton.addEventListener("click", () => {
@@ -38,3 +85,4 @@ modeToggle.addEventListener("click", () => {
         modeToggle.textContent = "Light Mode";
     }
 });
+
